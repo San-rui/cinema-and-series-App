2d128ad8cea82cb4a45396a5be64f23a325395ad
San <sruidiaz@hotmail.com>
2021-12-24 12:53:27 +0100

    current user fixed

--- src/redux/actions/usersList.ts
+++ src/redux/actions/usersList.ts
@@ -17,30 +17,32 @@ export const getUsersAction = () => {
     };
 };
 
+const getUsersStart = () => ({
+    type: types.getUsersStart,
+    payload: [],
+});
+
+const getUsersSuccess = (data: User[]) => ({
+    type: types.getUsersSuccess,
+    payload: data,
+});
+
+const getUsersError = (err: any) => ({
+    type: types.getUsersError,
+    payload: err.toString(),
+});
+
+
 export const addUserAction = (user: AddUserType) => {
     return async (dispatch: any) => {
         dispatch(getUsersStart());
 
         try {
-        await apiFirebase.post("/users.json", user);
+            await apiFirebase.post("/users.json", user);
         } catch (err) {
             dispatch(getUsersError(err));
         }
     };
 };
 
-const getUsersStart = () => ({
-    type: types.addUserstart,
-    payload: [],
-});
-
-const getUsersSuccess = (data: User[]) => ({
-    type: types.addUserSuccess,
-    payload: data,
-});
-
-const getUsersError = (err: any) => ({
-    type: types.addUserError,
-    payload: err.toString(),
-});
 
